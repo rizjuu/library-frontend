516ff5aabc009c6de51c7b3bca5452fe8fc39dcf
rizjuu <honculada.jeoriz@gmail.com>
2026-08-15 16:15:59 +0800

    et

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`
+const api = axios.create({
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
 });
+
+export default api;
